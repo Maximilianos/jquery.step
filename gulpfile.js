d2c4a41b242b9adc1df8fd0185598b330307c7c0
Max G J Panas <m@maxpanas.com>
2015-05-25 01:05:51 +0300

    add a gulp minification task

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp   = require('gulp');
+var uglify = require('gulp-uglify');
+var notify = require('gulp-notify');
+
+
+
+gulp.task('default', function () {
+  return gulp.src('step.jquery.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({
+      message: 'Yay! Plugin Minified!',
+      onLast: true
+    }));
+});
