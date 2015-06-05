649a06112265a141a6c3d304f65f74b5d450a9dd
Max G J Panas <m@maxpanas.com>
2015-06-06 02:40:05 +0300

    add gulp-rename and export both the minified and non minified js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp   = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var notify = require('gulp-notify');
 
 
 
 gulp.task('default', function () {
   return gulp.src('jquery.step.js')
+    .pipe(gulp.dest('dist'))
     .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('dist'))
     .pipe(notify({
       message: 'Yay! Plugin Minified!',
